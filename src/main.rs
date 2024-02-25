41deaaeef15e512bfb3de82370c6aca749dd38cc
Joao Paulo Alencar Rodrigues <joao.paulo@inovan.do>
2024-02-25 14:41:43 -0300

    prints up the browser request for more details on the web request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::net::TcpListener;
+use std::{io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9999").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+
+    println!("Ready to connect!");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection estabilished!")
+        handle_connections(stream)
     }
+}
+
+fn handle_connections(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader.lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
