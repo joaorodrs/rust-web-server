c99ab02e5fc1b16200555f006a066faac60eda8f
Joao Paulo Alencar Rodrigues <joao.paulo@inovan.do>
2024-02-25 15:07:16 -0300

    serves index.html file in response to localhost request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
@@ -19,7 +19,11 @@ fn handle_connections(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
